2cd0b218eb3464ab3df87475de8d3ab93577a20e
sunmooooon1023 <sunmooooon1023@gmail.com>
2025-08-19 16:38:19 +0900

    수정

--- js/search.js
+++ js/search.js
@@ -1,7 +1,7 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 6,
-    spaceBetween: 230,
     grabCursor: true,
+    centeredSlides: true, 
     loop: true,  // 무한 루프 활성화
     pagination: {
         el: ".swiper-pagination",
