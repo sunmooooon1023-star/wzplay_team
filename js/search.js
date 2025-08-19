5560c429890d8a67d89f9ada350c404942070feb
sunmooooon1023 <sunmooooon1023@gmail.com>
2025-08-19 18:11:09 +0900

    수정

--- js/search.js
+++ js/search.js
@@ -1,3 +1,26 @@
+
+/* header */
+
+fetch('./inner_header/header.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('main-header-container').innerHTML = data;
+  });
+
+const link1 = document.createElement('link');
+link1.rel = 'stylesheet';
+link1.href = './inner_header/css/index.css';
+document.head.appendChild(link1);
+
+const link2 = document.createElement('link');
+link2.rel = 'stylesheet';
+link2.href = './inner_header/css/layout.css';
+document.head.appendChild(link2);
+
+
+
+
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 6,
     grabCursor: true,
@@ -8,3 +31,5 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
+
+
