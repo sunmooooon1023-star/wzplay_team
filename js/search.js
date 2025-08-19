be19c0a240ec0c29a79d760601175eddfd9d6f03
sunmooooon1023 <sunmooooon1023@gmail.com>
2025-08-19 09:44:22 +0900

    8월 18일

---
+++ js/search.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+                    slidesPerView: 6,
+                    spaceBetween: 300,
+                    grabCursor: true,
+                    loop: true,  // 무한 루프 활성화
+                    pagination: {
+                        el: ".swiper-pagination",
+                        clickable: true,
+                    },
+                });
